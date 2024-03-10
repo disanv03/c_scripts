a67e16b3c9321d98b98f43205cfffb17a7054002
disanv03 <heddisanv@gmail.com>
2024-03-10 15:25:30 +0100

    next step low-level file copy using buffer

---
+++ let_us_c/chap19/low_lvl_copy.c
@@ -0,0 +1,15 @@
+/* A Low-Level File-Copy Program
+ * File-copy program which copies text & binary fi
+ *
+ */
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    char buffer[512], source[128], target[128];
+
+}
+
