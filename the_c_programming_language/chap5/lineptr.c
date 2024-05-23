498030df1367e3e98bcfe8221daeb5062e61a42b
disanv03 <heddisanv@gmail.com>
2024-05-23 15:18:41 +0200

    differences & similarities of arrays and pointers

---
+++ the_c_programming_language/chap5/lineptr.c
@@ -0,0 +1,60 @@
+/* pointer Arrays; Pointers to pointers
+ * The input collect and save the characers of each line, and build an
+ * array of pointers to the lines. It will also have to count the number
+ * of input lines, since that information is needed for sorting and printing
+ *
+ * The ouput routine has to print the lines in the order in which they appear
+ * in the array of pointers.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000           /* max #lines to be sorted      */
+char *lineptr[MAXLINES];        /* pointers to text lines       */
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+/* sort input lines     */
+main() {
+    int nlines;                 /* number of input lines read   */
+
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        qsort(lineptr, 0, nlines-1);
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+}
+
+#define MAXLEN 1000             /* max length of any input line */
+int getline(char *, int);
+char *alloc(int);
+
+/* readlines: read input lines */
+int readlines(char *lineptr[], int maxlines) {
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while ((len = getline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || p = alloc(len) == NULL)
+            return -1;
+        else {
+            line[len-1] = '\0'; /* delete newline       */
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+
+/* writelines: write ouput lines */
+void writelines(char *lineptr[], int nlines) {
+    int i;
+    for (i = 0; i < nlines; i++)
+        printf("%s\n", lineptr[i]);
+}
