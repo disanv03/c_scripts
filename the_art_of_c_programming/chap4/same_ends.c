ceb22dda49015e8649afb19ada48bc14e5275280
disanv03 <heddisanv@gmail.com>
2024-07-09 13:59:25 +0200

    same_ends exercice, logical operators

--- the_art_of_c_programming/chap4/same_ends.c
+++ the_art_of_c_programming/chap4/same_ends.c
@@ -1,7 +1,9 @@
 /* write a function same_end(p) that returns 1 if a byte p starts
  * and ends with the same bit, and 0 if not.
+ *
+ * check if the first and last bits of p are the same
  */
 
 int same_ends(p) {
-    return ((p & 129 == 0) | (p & 129 == 129));
+    return ((p & 129 == 0) || (p & 129 == 129));
 }
